d77a232577543331ae6e79e6da22dbc8116e4393
wattox00 <korschzozinho@gmail.com>
2024-09-27 19:36:15 +0200

    🎲🤷‍♀️🤔🤦‍♂️➡🐱‍🚀

--- dice.js
+++ dice.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
 const rollButton = document.getElementById("rollButton");
 const dice = document.getElementById("dice");
 const result = document.getElementById("result");
@@ -14,7 +15,23 @@ rollButton.addEventListener("click", () => {
     const betType = betTypeSelect.value;
 
     if (betAmount > balance) {
-        result.textContent = "Insufficient funds!";
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'block';
+
+        window.addEventListener('click', (event) => {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+        const closeModalButton = document.getElementsByClassName('close')[0];
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                modal.style.display = 'none';
+            }
+        });
         return;
     }
 
@@ -82,3 +99,4 @@ function updateBalance() {
     balanceElement.textContent = balance;
     localStorage.setItem('balance', balance);
 }
+})
--- dice2.js
+++ dice2.js
@@ -37,7 +37,23 @@ function rollDice() {
     const rollUnder = parseInt(document.getElementById('roll-slider').value);
     
     if (betAmount > balance) {
-        alert('You don\'t have enough balance!');
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'block';
+
+        window.addEventListener('click', (event) => {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+        const closeModalButton = document.getElementsByClassName('close')[0];
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                modal.style.display = 'none';
+            }
+        });
         return;
     }
 
